3d78f275fde8213f9c7c0b34d276487ad93ea086
Kristof Poszet <kristof_poszet@epam.com>
2022-01-06 13:49:21 +0100

    change CSS injection order

--- mui-practice/src/index.js
+++ mui-practice/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Change the CSS injection order */}
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
